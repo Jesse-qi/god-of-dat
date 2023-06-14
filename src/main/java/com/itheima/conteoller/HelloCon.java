146814abfbda80037c24818c23f11aee30d8063a
longer <01111@.com>
2023-06-14 22:32:51 +0800

    Merge branch 'master' of https://gitee.com/qi-longyu/sdata # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/main/java/com/itheima/conteoller/HelloCon.java
+++ src/main/java/com/itheima/conteoller/HelloCon.java
@@ -4,5 +4,7 @@ public class HelloCon {
     public static void main(String[] args) {
         System.out.println("sdata");
         System.out.println(45);
+
+        System.out.println("cahgngneioa");
     }
 }
